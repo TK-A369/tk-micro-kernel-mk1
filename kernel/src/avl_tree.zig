73edafdd952a6dbec52add3e7684301a1fd7ce31
Tomasz Kawalec <tkawalec369@gmail.com>
2025-09-23 20:26:48 +0200

    Perform rotations in AvlTree.insert method - but heights aren't properly updated in some places yet

--- kernel/src/avl_tree.zig
+++ kernel/src/avl_tree.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const misc = @import("misc.zig");
+
 /// This isn't the optimal implementation, because we perform an allcoation for every node, and they might therefore be spread out in the memory
 pub fn AvlTree(comptime T: type) type {
     return struct {
@@ -37,21 +39,97 @@ pub fn AvlTree(comptime T: type) type {
                 .parent = parent_node,
             };
 
+            // Perform balancing
             var backtracking_curr_node = curr_node_ptr.*;
             while (backtracking_curr_node) |backtracking_curr_node_nn| {
-                const left_height = if (backtracking_curr_node_nn.left) |bcnl| {
+                const left_height: u64 = if (backtracking_curr_node_nn.left) |bcnl| {
                     break bcnl.height;
                 } else {
                     break 0;
                 };
-                const right_height = if (backtracking_curr_node_nn.right) |bcnr| {
+                const right_height: u64 = if (backtracking_curr_node_nn.right) |bcnr| {
                     break bcnr.height;
                 } else {
                     break 0;
                 };
+                const bf: i64 = right_height - left_height;
+                // If necessary, perform rotation
+                if (bf >= 2) {
+                    if (backtracking_curr_node_nn.right) |bcnr| {
+                        //     A
+                        //  B     C
+                        //       D E
+                        // ==========>
+                        //     C
+                        //  A     E
+                        // B D
+                        const node_a_val = backtracking_curr_node_nn.value;
+                        const node_c_val = bcnr.value;
+                        const node_b = bcnr.backtracking_curr_node_nn.left;
+                        const node_d = bcnr.left;
+                        const node_e = bcnr.right;
+                        // Node C
+                        backtracking_curr_node_nn.value = node_c_val;
+                        backtracking_curr_node_nn.left = bcnr;
+                        bcnr.parent = backtracking_curr_node_nn;
+                        backtracking_curr_node_nn.right = node_e;
+                        if (node_e) |node_e_nn| {
+                            node_e_nn.parent = backtracking_curr_node_nn;
+                        }
+                        // Node A
+                        bcnr.value = node_a_val;
+                        bcnr.left = node_b;
+                        if (node_b) |node_b_nn| {
+                            node_b_nn.parent = bcnr;
+                        }
+                        bcnr.right = node_d;
+                        if (node_d) |node_d_nn| {
+                            node_d_nn.parent = bcnr;
+                        }
+                    } else {
+                        // This shouldn't ever happen
+                        misc.hcf();
+                    }
+                } else if (bf <= -2) {
+                    if (backtracking_curr_node_nn.left) |bcnl| {
+                        //     A
+                        //  B     C
+                        // D E
+                        // ==========>
+                        //     B
+                        //  D     A
+                        //       E C
+                        const node_a_val = backtracking_curr_node_nn.value;
+                        const node_b_val = bcnl.value;
+                        const node_c = backtracking_curr_node_nn.right;
+                        const node_d = bcnl.left;
+                        const node_e = bcnl.right;
+                        // Node B
+                        backtracking_curr_node_nn.value = node_b_val;
+                        backtracking_curr_node_nn.left = node_d;
+                        if (node_d) |node_d_nn| {
+                            node_d_nn.parent = backtracking_curr_node_nn;
+                        }
+                        backtracking_curr_node_nn.right = bcnl;
+                        bcnl.parent = backtracking_curr_node_nn;
+                        // Node A
+                        bcnl.value = node_a_val;
+                        bcnl.left = node_e;
+                        if (node_e) |node_e_nn| {
+                            node_e_nn.parent = bcnl;
+                        }
+                        bcnl.right = node_c;
+                        if (node_c) |node_c_nn| {
+                            node_c_nn.parent = bcnl;
+                        }
+                    } else {
+                        // This shouldn't ever happen
+                        misc.hcf();
+                    }
+                }
                 backtracking_curr_node_nn.height = 1 + @max(
-                    (backtracking_curr_node_nn.left orelse (&0)).*,
-                    (backtracking_curr_node_nn.right orelse (&0)).*,
+                    left_height,
+                    right_height,
                 );
                 backtracking_curr_node = backtracking_curr_node_nn.parent;
             }
