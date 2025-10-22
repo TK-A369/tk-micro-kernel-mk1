52d6dde59d6dd3377a0b2b6e1d679b606950055a
Tomasz Kawalec <tkawalec369@gmail.com>
2025-10-22 22:47:29 +0200

    Start rewriting AvlTree.delete to be recursive

--- kernel/src/avl_tree.zig
+++ kernel/src/avl_tree.zig
@@ -44,62 +44,6 @@ pub fn AvlTree(comptime T: type) type {
             }
         }
 
-        pub fn insert(self: *This, value: *T, curr_node: ?*Node, parent_node: ?*Node) *Node {
-            if (curr_node) |curr_node_nn| {
-                if (self.cmp_fn(value, curr_node_nn.value)) {
-                    curr_node_nn.left = self.insert(value, curr_node_nn.left, curr_node_nn);
-                } else {
-                    curr_node_nn.right = self.insert(value, curr_node_nn.rigth, curr_node_nn);
-                }
-                updateHeight(curr_node_nn);
-
-                const bf = @as(i64, getHeight(curr_node_nn.left)) - @as(i64, getHeight(curr_node_nn.right));
-                if (bf >= 2) {
-                    // Left-heavy
-                    if (self.cmp_fn(curr_node_nn.left.?, value)) {
-                        self.rotateLeft(curr_node_nn.left.?);
-                        return self.rotateRight(curr_node_nn);
-                    } else {
-                        return rotateRight(curr_node_nn);
-                    }
-                } else if (bf <= -2) {
-                    // Right-heavy
-                    if (self.cmp_fn(value, curr_node.right.?.data)) {
-                        rotateRight(curr_node_nn.right);
-                        return rotateLeft(curr_node_nn);
-                    } else {
-                        return rotateLeft(curr_node_nn);
-                    }
-                } else {
-                    return curr_node_nn;
-                }
-            } else {
-                curr_node = self.allocator.create(Node);
-                curr_node.?.* = .{
-                    .value = value,
-                    .height = 1,
-                    .left = null,
-                    .right = null,
-                    .parent = parent_node,
-                };
-                return curr_node.?;
-            }
-        }
-
-        pub fn search(self: *This, value: *T) ?*Node {
-            var curr_node: ?*Node = self.root;
-            while (curr_node) |curr_node_nn| {
-                if (self.cmp_fn(curr_node_nn.value, value)) {
-                    curr_node = curr_node_nn.right;
-                } else if (self.cmp_fn(value, curr_node_nn.value)) {
-                    curr_node = curr_node_nn.left;
-                } else {
-                    return curr_node_nn;
-                }
-            }
-            return null;
-        }
-
         // From:
         //     A
         //  e     B
@@ -179,7 +123,49 @@ pub fn AvlTree(comptime T: type) type {
             }
         }
 
-        pub fn delete(self: *This, value: *T) void {
+        pub fn insert(self: *This, value: *T, curr_node: ?*Node, parent_node: ?*Node) *Node {
+            if (curr_node) |curr_node_nn| {
+                if (self.cmp_fn(value, curr_node_nn.value)) {
+                    curr_node_nn.left = self.insert(value, curr_node_nn.left, curr_node_nn);
+                } else {
+                    curr_node_nn.right = self.insert(value, curr_node_nn.rigth, curr_node_nn);
+                }
+                updateHeight(curr_node_nn);
+
+                const bf = @as(i64, getHeight(curr_node_nn.left)) - @as(i64, getHeight(curr_node_nn.right));
+                if (bf >= 2) {
+                    // Left-heavy
+                    if (self.cmp_fn(curr_node_nn.left.?, value)) {
+                        self.rotateLeft(curr_node_nn.left.?);
+                        return self.rotateRight(curr_node_nn);
+                    } else {
+                        return rotateRight(curr_node_nn);
+                    }
+                } else if (bf <= -2) {
+                    // Right-heavy
+                    if (self.cmp_fn(value, curr_node.right.?.data)) {
+                        rotateRight(curr_node_nn.right);
+                        return rotateLeft(curr_node_nn);
+                    } else {
+                        return rotateLeft(curr_node_nn);
+                    }
+                } else {
+                    return curr_node_nn;
+                }
+            } else {
+                curr_node = self.allocator.create(Node);
+                curr_node.?.* = .{
+                    .value = value,
+                    .height = 1,
+                    .left = null,
+                    .right = null,
+                    .parent = parent_node,
+                };
+                return curr_node.?;
+            }
+        }
+
+        pub fn search(self: *This, value: *T) ?*Node {
             var curr_node: ?*Node = self.root;
             while (curr_node) |curr_node_nn| {
                 if (self.cmp_fn(curr_node_nn.value, value)) {
@@ -187,84 +173,28 @@ pub fn AvlTree(comptime T: type) type {
                 } else if (self.cmp_fn(value, curr_node_nn.value)) {
                     curr_node = curr_node_nn.left;
                 } else {
-                    const left_height: u64 = left_height_blk: {
-                        if (curr_node_nn.left) |cnl| {
-                            break :left_height_blk cnl.height;
-                        } else {
-                            break :left_height_blk 0;
-                        }
-                    };
-                    const right_height: u64 = right_height_blk: {
-                        if (curr_node_nn.right) |cnr| {
-                            break :right_height_blk cnr.height;
-                        } else {
-                            break :right_height_blk 0;
-                        }
-                    };
+                    return curr_node_nn;
+                }
+            }
+            return null;
+        }
 
-                    // 0 - there are no children to pull up
-                    // -1 - pull up left
-                    // 1 - pull up right
-                    var pull_up_which: i8 = 0;
-                    if (left_height > right_height) {
-                        if (curr_node_nn.right) {
-                            pull_up_which = 1;
-                        } else if (curr_node_nn.left) {
-                            pull_up_which = -1;
+        pub fn delete(self: *This, value: *T, curr_node: ?*Node) !?*Node {
+            if (curr_node) |curr_node_nn| {
+                if (self.cmp_fn(value, curr_node_nn.data)) {
+                    curr_node_nn.left = self.delete(value, curr_node_nn.left);
+                } else if (self.cmp_fn(curr_node_nn.data, value)) {
+                    curr_node_nn.right = self.devele(value, curr_node_nn.right);
+                } else {
+                    // TODO
+                    if (curr_node_nn.left) |left_nn| {} else {
+                        if (curr_node_nn.right) |right_nn| {} else {
+                            self.allocator.destroy(curr_node_nn);
                         }
-                    } else {
-                        if (curr_node_nn.left) {
-                            pull_up_which = -1;
-                        } else if (curr_node_nn.right) {
-                            pull_up_which = 1;
-                        }
-                    }
-
-                    // 0 - this is a root node
-                    // -1 - this node is left child
-                    // 1 - this node is right child
-                    const parent_side: i8 = parent_side_blk: {
-                        if (curr_node_nn.parent) |parent_nn| {
-                            if (parent_nn.left == curr_node_nn) {
-                                break :parent_side_blk -1;
-                            } else if (parent_nn.right == curr_node_nn) {
-                                break :parent_side_blk 1;
-                            }
-                        }
-                        break :parent_side_blk 0;
-                    };
-                    switch (pull_up_which) {
-                        -1 => {
-                            switch (parent_side) {
-                                -1 => {
-                                    curr_node_nn.parent.left = curr_node_nn.left.?;
-                                },
-                                0 => {
-                                    self.root = curr_node_nn.left.?;
-                                },
-                                1 => {
-                                    curr_node_nn.parent.right = curr_node_nn.left.?;
-                                },
-                            }
-                            // curr_node_nn.
-                        },
-                        0 => {
-                            switch (parent_side) {
-                                -1 => {
-                                    curr_node.parent.left = null;
-                                },
-                                0 => {
-                                    self.root = null;
-                                },
-                                1 => {
-                                    curr_node.parent.right = null;
-                                },
-                            }
-                            self.allocator.destroy(curr_node);
-                        },
-                        1 => {},
                     }
                 }
+            } else {
+                return null;
             }
         }
     };
